03d2d00728977888d9786eee44d163ce64f7c5d0
Mateus <edwardaleatorio@gmail.com>
2026-04-23 21:13:45 -0300

    LivroExemplarController criado

--- src/main/java/com/biblioteca/Biblioteca/controller/LivroExemplarController.java
+++ src/main/java/com/biblioteca/Biblioteca/controller/LivroExemplarController.java
@@ -1,23 +1,27 @@
 package com.biblioteca.Biblioteca.controller;
 
 import com.biblioteca.Biblioteca.DTO.ExemplarDto;
+import com.biblioteca.Biblioteca.DTO.LivroDto;
 import com.biblioteca.Biblioteca.service.ExemplarService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
+@RequestMapping("/livros/{id}")
 @RestController
-@RequestMapping("/exemplar")
-public class ExemplarController {
+public class LivroExemplarController {
     private final ExemplarService exemplarService;
 
+
+
     @Autowired
-    public ExemplarController(ExemplarService exemplarService) {
+    public LivroExemplarController (ExemplarService exemplarService) {
         this.exemplarService = exemplarService;
     }
 
-    @PostMapping("/{quantidade}")
+    @PostMapping("/exemplar/{quantidade}")
     public ResponseEntity<List<ExemplarDto>> criarExemplares(@PathVariable int quantidade, @RequestBody ExemplarDto exemplarDto) {
         List<ExemplarDto> exemplaresCriados = exemplarService.criarExemplares(exemplarDto, quantidade);
 
